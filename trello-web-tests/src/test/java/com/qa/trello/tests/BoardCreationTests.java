5270a0f7818a4ea0fb554f658562575f5edb7f82
Pima1986 <36693464+Pima1986@users.noreply.github.com>
2020-04-16 16:25:37 +0300

    wev-trello-tests

---
+++ trello-web-tests/src/test/java/com/qa/trello/tests/BoardCreationTests.java
@@ -0,0 +1,79 @@
+package com.qa.trello.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class BoardCreationTests {
+    WebDriver wd;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp() {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        wait = new WebDriverWait(wd, 17);
+        wd.manage().window().maximize();
+        wd.navigate().to("https://trello.com/");
+    }
+
+    @Test
+    public void testBoardCreation() {
+
+        //1. initLogin
+        initLogin();
+
+        //2. fillLoginFormAtlassian
+        fillLoginFormAtlassian("daniel.pimshteyn@gmail.com", "Qwerty12345");
+
+        //3. confirmLogin
+        confirmLogin();
+
+        //4. init board creation
+        //5. fill board form
+        //6. confirm board creation
+        //7. return to home page
+    }
+
+    private void confirmLogin() {
+        //wait.until(presenceOfElementLocated(By.id("login-submit"))).click();
+        click(By.id("login-submit"));
+    }
+
+    private void fillLoginFormAtlassian(String userEmail, String password) {
+        type(By.name("user"), userEmail);
+        click(By.cssSelector("#login.button-green"));
+        type(By.id("password"), password);
+    }
+
+    private void initLogin() {
+        click(By.cssSelector("[href='/login']"));
+    }
+
+    private void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    private void click(By locator) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator)).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+
+    
+}
