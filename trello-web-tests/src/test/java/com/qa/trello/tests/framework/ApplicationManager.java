700ff899e6149e106034a773d50b9a113e1d9092
Pima1986 <36693464+Pima1986@users.noreply.github.com>
2020-05-02 14:15:44 +0300

    change browser

--- trello-web-tests/src/test/java/com/qa/trello/tests/framework/ApplicationManager.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/framework/ApplicationManager.java
@@ -2,6 +2,8 @@ package com.qa.trello.tests.framework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -12,8 +14,20 @@ public class ApplicationManager {
     GroupHelper group;
     SessionHelper session;
     private WebDriverWait wait;
+    String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
 
     public void init() throws InterruptedException {
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        }
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        }
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         wait = new WebDriverWait(wd, 20);
