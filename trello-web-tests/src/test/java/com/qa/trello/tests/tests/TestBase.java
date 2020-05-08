5f52236a9497c3e219863ff945c46cb520f7e3f9
Pima1986 <36693464+Pima1986@users.noreply.github.com>
2020-05-08 14:57:34 +0300

    added logging

--- trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
@@ -2,8 +2,17 @@ package com.qa.trello.tests.tests;
 
 import com.qa.trello.tests.framework.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 
 public class TestBase {
 
@@ -11,11 +20,22 @@ public class TestBase {
     protected static ApplicationManager app = new ApplicationManager(
             System.getProperty("browser", BrowserType.CHROME));
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     @BeforeSuite
     public void setUp() throws InterruptedException {
         app.init();
 
+    }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test "+ m.getName() + " with parameters: " + Arrays.asList(p));
+    }
+
+    @AfterMethod
+    public void logTestStop(Method m) {
+        logger.info("Stop test "+m.getName());
     }
 
     @AfterSuite
